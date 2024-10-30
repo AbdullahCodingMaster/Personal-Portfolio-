2442babfb2d4d8d207eed01b9a60b32024f6a5d1
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-10-30 08:47:54 +0500

    tweking some thing

--- src/components/ProjectItems.jsx
+++ src/components/ProjectItems.jsx
@@ -7,10 +7,10 @@ const ProjectItems = ({ img, title, githubLink, liveLink, technologies }) => {
       <img
         src={img}
         alt={title}
-        className="w-full h-64 object-cover rounded-t-lg group-hover:opacity-10 transition-opacity duration-300"
+        className="w-full h-64 object-cover rounded-t-lg opacity-10 transition-opacity duration-300"
       />
 
-      <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
+      <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
         <h3 className="text-2xl font-semibold text-white mb-2">{title}</h3>
         <p className="text-gray-300 text-center mb-4">{technologies}</p>
 
@@ -19,7 +19,7 @@ const ProjectItems = ({ img, title, githubLink, liveLink, technologies }) => {
             href={githubLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center bg-blue-600 hover:bg-blue-500 text-white py-2 px-4 rounded-lg transition"
+            className="flex items-center bg-blue-600 text-white py-2 px-4 rounded-lg transition duration-300"
           >
             <FaGithub size={20} className="mr-2" />
             GitHub
@@ -29,7 +29,7 @@ const ProjectItems = ({ img, title, githubLink, liveLink, technologies }) => {
             href={liveLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center bg-blue-400 hover:bg-blue-300 text-white py-2 px-4 rounded-lg transition"
+            className="flex items-center bg-blue-400 text-white py-2 px-4 rounded-lg transition duration-300"
           >
             <FaGlobe size={20} className="mr-2" />
             Live Site
